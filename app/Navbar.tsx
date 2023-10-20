64c529962f02044a140115ff570486a30f572105
chandrainf <surel.chandrapratama@gmail.com>
2023-10-20 11:11:29 +0700

    styling use client

--- app/Navbar.tsx
+++ app/Navbar.tsx
@@ -1,13 +1,21 @@
+'use client';
+
 import Link from 'next/link'
+import { usePathname } from 'next/navigation';
 import React from 'react'
 import { AiFillBug } from 'react-icons/ai';
 
 
 const Navbar = () => {
+
+    const currentPath = usePathname();
+    console.log(currentPath);
+
     const links = [
         { label: 'Dashboard', href: '/'},
         { label: 'Issues', href: '/issues'},
     ]
+
     return (
         <nav className='flex space-x-6 border-b mb-5 px-5 h-14 items-center'>
             <Link href="/"><AiFillBug /></Link>
@@ -15,7 +23,7 @@ const Navbar = () => {
                 {links.map(link =>
                  <Link 
                  key={link.href}
-                 className='text-zinc-500' 
+                 className={`${link.href === currentPath ? 'text-zinc-900' : 'text-zinc-500'} hover:text-zinc-800 transition-colors`} 
                  href={link.href}>{link.label}</Link>)}
                 
                 
